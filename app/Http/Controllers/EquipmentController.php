e3c45a1472eabca6b7dd2e6a0987f2403a4d6471
Alex Hernandez <55168529+ahernandez577@users.noreply.github.com>
2022-03-30 16:38:25 -0700

    dam

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -16,7 +16,7 @@ class EquipmentController extends Controller
      */
     public function index()
     {
-        $equipments = Equipment::all();
+        $equipment = Equipment::all();
         return view('equipment.list', compact('equipment'));
     }
 
