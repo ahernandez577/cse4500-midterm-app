7d98229fd5a27abf7868759da0fc83ff79b37c1d
Alex Hernandez <55168529+ahernandez577@users.noreply.github.com>
2022-03-30 14:01:23 -0700

    nice

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Kris\LaravelFormBuilder\FormBuilderServiceProvider::class
+        Kris\LaravelFormBuilder\FormBuilderServiceProvider::class,
 
         /*
          * Application Service Providers...
