8c0d615a37781f1160196fb289de3bde2c05c58c
Alex Hernandez <55168529+ahernandez577@users.noreply.github.com>
2022-03-30 13:58:45 -0700

    a

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Kris\LaravelFormBuilder\FormBuilderServiceProvider::class
 
         /*
          * Application Service Providers...
@@ -192,6 +193,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // ...
+        'FormBuilder' => Kris\LaravelFormBuilder\Facades\FormBuilder::class
     ])->toArray(),
 
 ];
